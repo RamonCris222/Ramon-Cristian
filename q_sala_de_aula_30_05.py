f2ed74069b854da397f2e58170546e43e0c2f846
RamonCris222 <cristianramon123@hotmail.com>
2017-05-31 11:14:15 -0300

    Delete q_sala_de_aula_30_05.py

--- q_sala_de_aula_30_05.py
+++
@@ -1,8 +0,0 @@
-try:
-
-    x = int(input("Digite um número:"))
-    y = int(input("Digite outro número:"))
-    print("Os números informados são:", x, "e", y)
-
-except:
-    print("Você digitou algo errado!")
